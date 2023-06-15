4114e22d5e3acc401532426ca4216ca53bd5a346
Pavel <bezukladnikov.pavel.be@gmail.com>
2023-06-15 16:33:13 +0200

    The Personnes service is complete.

--- controllers/personnes.controller.js
+++ controllers/personnes.controller.js
@@ -6,7 +6,7 @@ const { ErrorResponse } = require('../utils/ErrorResponse')
 
 
 
-//TODO Gestion de l'Error response
+
 //TODO Verifier le statusCode
 const personneController = {
     /**
@@ -25,22 +25,39 @@ const personneController = {
     },
 
     /**
-     * getByID
-     * @param { Request } req
-     * @param { Response } res
-     */
-    getByID: async ( req, res ) => {
-        res.sendStatus(501)
-    },
+    * getByID
+    * @param { Request } req
+    * @param { Response } res
+    */
+   getByID: async ( req, res ) => {
+       const { values, count } = await personneService.getByParams(req.params)
+
+       if(values)
+           if( values.length > 0 )
+               res.status(200).json(new SuccessResponse( values, count ))
+           else 
+               res.status(200).json(new SuccesResponseMsg('The element was not found.', 200))
+       else 
+           res.status(400).json(new ErrorResponse('The elements were not found.', 400))
+   },
+
 
     /**
-     * GetByParams
+     * getByParams
      * @param { Request } req
      * @param { Response } res
      */
     getByParams: async ( req, res ) => {
-        res.sendStatus(501)
-        
+        console.log(req.params);
+        const { values, count } = await personneService.getByParams(req.query)
+
+        if(values)
+            if( values.length > 0 )
+                res.status(200).json(new SuccessResponse( values, count ))
+            else 
+                res.status(200).json(new SuccesResponseMsg('The element was not found.', 200))
+        else 
+            res.status(400).json(new ErrorResponse('The elements were not found.', 400))
     },
 
     /**
@@ -64,8 +81,66 @@ const personneController = {
      * @param { Response } res
      */
     update: async ( req, res ) => {
-        res.sendStatus(501)
-        //TODO faire l'update 
+    
+        const id = req.params.ID_Personne
+        const body = req.body
+
+        const updatePersonne = await personneService.update( id, body )
+
+        if(updatePersonne)
+            res.status(200).json(new SuccesResponseMsg('The update is successful.', 200))
+        else
+            res.status(400).json(new ErrorResponse('An error occurred during the update.', 400))      
+    },
+
+    removeMovieFromActors: async (req, res) => {
+
+        const id = req.params.ID_Personne
+        const body = req.body.movies
+
+        const removeMoviesFromActors = await personneService.removeMovieFromActors(id, body)
+        if (removeMoviesFromActors)
+            if (removeMoviesFromActors === 'NotInRange')
+                res.status(200).json(new SuccesResponseMsg('The element to be removed is not found in the array.', 200))
+            else
+                res.status(200).json(new SuccesResponseMsg('The update is successful.', 200))
+        else
+            res.status(400).json(new ErrorResponse('An error occurred during the update.', 400))
+    },
+
+    removeMovieFromWriters: async (req, res) => {
+
+        const id = req.params.ID_Personne
+        const body = req.body.movies
+
+        const removeMoviesFromWriters = await personneService.removeMovieFromWriters(id, body)
+        if (removeMoviesFromWriters)
+            if (removeMoviesFromWriters === 'NotInRange')
+                res.status(200).json(new SuccesResponseMsg('The element to be removed is not found in the array.', 200))
+            else
+                res.status(200).json(new SuccesResponseMsg('The update is successful.', 200))
+        else
+            res.status(400).json(new ErrorResponse('An error occurred during the update.', 400))
+    },
+
+
+    /**
+     * update Avatar
+     * @param { Request } req
+     * @param { Response } res
+     */
+    updateAvatar: async (req, res) => {
+        
+        console.log(req.file.filename);
+        const id = req.params.ID_Personne
+        console.log("id :", id);
+        const filename = req.file.filename
+
+        const updated = await personneService.updateAvatar(id, filename)
+        if (!updated) {
+            return res.sendStatus(400)
+        }
+        res.sendStatus(204)
     },
 
     /**
--- routes/personnes.router.js
+++ routes/personnes.router.js
@@ -1,16 +1,31 @@
 const personneController = require('../controllers/personnes.controller')
 const authRoles = require('./../middlewares/authRoles')
+const bodyValidator = require ('../middlewares/bodyValidator.js')
 
 const  personneRouter = require('express').Router()
 
+
+const multer = require('multer')
+const { createPersonnesValidator, updatePersonnesValidator, removeActorsPersonnesValidator, removeWritersPersonnesValidator } = require('../Validators/personnes.validator')
+console.log("test");
+const storage = require('../utils/config.multer')('personnesCovers')
+console.log("test2");
+const upload = multer({storage})
+console.log("test3");
+
 personneRouter.route('/')
     .get(personneController.getAll)
-    .post(authRoles('Admin'),personneController.create)
+    .post(authRoles('Admin'), bodyValidator(createPersonnesValidator), personneController.create)
 personneRouter.route('/params')
     .get(personneController.getByParams)
+personneRouter.route('/removeActors/:ID_Personne')
+    .put(authRoles('Admin'), bodyValidator(removeActorsPersonnesValidator), personneController.removeMovieFromActors)
+personneRouter.route('/removeWriters/:ID_Personne')
+    .put(authRoles('Admin'), bodyValidator(removeWritersPersonnesValidator), personneController.removeMovieFromWriters)
 personneRouter.route('/:ID_Personne')
     .get(personneController.getByID)
-    .put(authRoles('Admin'),personneController.update)
+    .put(authRoles('Admin'), bodyValidator(updatePersonnesValidator), personneController.update)
+    .patch(authRoles('Admin'),upload.single('personnesCovers'),personneController.updateAvatar)
     .delete(authRoles('Admin'),personneController.delete)
 
 module.exports = personneRouter
